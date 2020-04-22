208f5bba1ab15d0ca3bb3c45802dcef2184148be
Kacper Dąbrowski <dombro22@gmail.com>
2020-04-22 12:10:31 +0200

    poprawka

--- Labolatorium_7/Punkty_Wektory/Punkt3D.java
+++ Labolatorium_7/Punkty_Wektory/Punkt3D.java
@@ -4,7 +4,7 @@ public class Punkt3D extends Punkt2D{
 
     Double z;
 
-    void setxyz(Double x, Double y)
+    public void setxyz(Double x, Double y, Double z)
     {
         this.x = x;
         this.y = y;
