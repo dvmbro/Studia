772aed4a6a16dc76cac2b01e3445752fdf7869ff
Kacper Dąbrowski <dombro22@gmail.com>
2020-04-22 12:58:55 +0200

    Administracja

---
+++ Labolatorium_7/Administracja/JednostkaAdministracyjna.java
@@ -0,0 +1,23 @@
+package Labolatorium_7.Administracja;
+
+public class JednostkaAdministracyjna {
+
+    String nazwaJednostki;
+    int liczebnosc;
+
+    public String getNazwaJednostki() {
+        return nazwaJednostki;
+    }
+
+    public void setNazwaJednostki(String nazwaJednostki) {
+        this.nazwaJednostki = nazwaJednostki;
+    }
+
+    public int getLiczebnosc() {
+        return liczebnosc;
+    }
+
+    public void setLiczebnosc(int liczebnosc) {
+        this.liczebnosc = liczebnosc;
+    }
+}
---
+++ Labolatorium_7/Administracja/Powiat.java
@@ -0,0 +1,23 @@
+package Labolatorium_7.Administracja;
+
+public class Powiat {
+
+    String nazwaPowiatu;
+    int liczebnośćPowiatu;
+
+    public String getNazwaPowiatu() {
+        return nazwaPowiatu;
+    }
+
+    public void setNazwaPowiatu(String nazwaPowiatu) {
+        this.nazwaPowiatu = nazwaPowiatu;
+    }
+
+    public int getLiczebnośćPowiatu() {
+        return liczebnośćPowiatu;
+    }
+
+    public void setLiczebnośćPowiatu(int liczebnośćPowiatu) {
+        this.liczebnośćPowiatu = liczebnośćPowiatu;
+    }
+}
